3f1715ac9285f2b4133f3f6bcb87d2d47503bdd4
brightblueray <rryjewski@hashicorp.com>
2022-09-26 15:38:18 -0400

    this is why I'm not a dev

--- rg.tf
+++ rg.tf
@@ -14,12 +14,12 @@ resource "random_id" "rg_name" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${random_pet.rg_name.id}-rg"
+  name     = "${random_id.rg_name.id}-rg"
   location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${random_pet.rg_name.id}-network"
+  name                = "${random_id.rg_name.id}-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -33,7 +33,7 @@ resource "azurerm_subnet" "intsubnet" {
 }
 
 resource "azurerm_network_security_group" "sg" {
-  name                = "${random_pet.rg_name.id}-sg"
+  name                = "${random_id.rg_name.id}-sg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
